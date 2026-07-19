c7eb6ff948ec99a5f0bcae70377aef6049f51c60
[Ali Ahmad] <[ahmadultani2004@gmail.com]>
2026-07-19 09:32:45 +0500

    rrls.py in sales section updated

--- smart retail/apps/sales/urls.py
+++ smart retail/apps/sales/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import SaleViewSet, CouponViewSet
+from .views import SaleViewSet, CouponViewSet, SaleReturnViewSet
 
 app_name = "sales"
 
 router = DefaultRouter()
 router.register("coupons", CouponViewSet, basename="coupon")
+router.register("returns", SaleReturnViewSet, basename="sale-return")
 router.register("", SaleViewSet, basename="sale")
 
 urlpatterns = router.urls
